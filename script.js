83a9c1a11c28270604568fbdc2968ef616a8bfab
Robinson Mourão <49078615+robinsonmourao@users.noreply.github.com>
2022-04-28 16:41:19 -0300

    Apenas praticando
    Adicionada classes por linha;
    linha 1 alinhado a direita via transform;

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function inserir(numeroInsertInput){
     var numero = document.getElementById("resultado").innerHTML;
     document.getElementById('resultado').innerHTML = numero + numeroInsertInput;
-    /* Não pode pega pelo nome pois tem que ser um numero */
+}
+function voltar(){
+    var numeroVoltar = document.getElementById("resultado").innerHTML;
+    document.getElementById("resultado").innerHTML = numeroVoltar.substring(0, numeroVoltar.length -1);
 }
 function limpar(){
     document.getElementsById('resultado').innerHTML = "";
 }
-function voltar(){
-    var numeroVoltar = document.getElementById("resultado").innerHTML;
-    document.getElementById("resultado").innerHTML = numeroVoltar.substring(0, numeroVoltar.length - 1);
-}
 function calcular(){
     var resultadoCalcular = document.getElementsById('resultado').innerHTML;
+
     if(resultadoCalcular){
         document.getElementsById('resultado').innerHTML = eval(resultadoCalcular);
     } else {
